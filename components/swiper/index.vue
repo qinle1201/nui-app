b53a8e052c87bab8d81f12a70ab797759a747407
秦乐 <8085524+qin-music@user.noreply.gitee.com>
2020-12-23 14:40:16 +0800

    完成了轮播图模块

---
+++ components/swiper/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="m-2" style="background-color: red;">
+		<swiper indicator-dots autoplay circular :interval="5000" indicator-active-color="#fff" :duration="2000">
+			<swiper-item v-for="(item,index) in imgArr" :key="index">
+				<image :src="item.src" mode="widthFix" class="w-100"></image>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			imgArr:{
+				type:Array,
+				default:()=>[]
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,20 +4,38 @@
 		<view :style="{height : `${statusBarHeight}px`}"></view>
 		<!-- 搜索布局 -->
 		<search></search>
+		<!-- 轮播图模块 -->
+		<my-swiper :imgArr="swiperImages"></my-swiper>
 	</view>
 </template>
 
 <script>
-	// import myIcon from '../../components/icon/index.vue'
 	import search from '../../components/search/index.vue'
+	import mySwiper from '../../components/swiper/index.vue'
 	export default {
 		components: {
-			// myIcon
-			search
+			search,
+			mySwiper
 		},
 		data() {
 			return {
-				statusBarHeight: this.$statusBarHeight
+				statusBarHeight: this.$statusBarHeight,
+				swiperImages: [{
+						src: "/static/swiperImages/swipertab1.png"
+					},
+					{
+						src: "/static/swiperImages/swipertab2.png"
+					},
+					{
+						src: "/static/swiperImages/swipertab3.png"
+					},
+					{
+						src: "/static/swiperImages/swipertab4.png"
+					},
+					{
+						src: "/static/swiperImages/swipertab5.png"
+					}
+				]
 			}
 		},
 		onLoad() {},
