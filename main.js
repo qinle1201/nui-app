32359332e7eba1586edb8f1a6a1cdc52a779cf96
秦乐 <8085524+qin-music@user.noreply.gitee.com>
2020-12-23 11:48:33 +0800

    设置顶部标题栏被覆盖问题

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+//将顶部通知栏的⾼度挂载全局,⽅便其他⻚⾯调⽤
+Vue.prototype.$statusBarHeight = uni.getSystemInfoSync().statusBarHeight;
+
 const app = new Vue({
     ...App
 })
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,31 +1,29 @@
 <template>
 	<view class="content">
+		<view :style="{height : `${statusBarHeight}px`}"></view>
 		<view class="bg-danger h5">hello world</view>
 		<text class="iconfont icon-shoucang" style="font-size:200rpx"></text>
 		<my-icon iconName="icon-bofangsanjiaoxing" iconSize="100rpx"></my-icon>
-		<view style="width:500rpx; height: 500rpx; background-color: green;"
-		class="animated" hover-class="flip"></view>
+		<view style="width:500rpx; height: 500rpx; background-color: green;" class="animated" hover-class="flip"></view>
 	</view>
 </template>
 
 <script>
 	import myIcon from '../../components/icon/index.vue'
 	export default {
-		components:{
+		components: {
 			myIcon
 		},
 		data() {
 			return {
+				statusBarHeight : this.$statusBarHeight
 			}
 		},
-		onLoad() {
-		},
-		methods: {
-		}
+		onLoad() {},
+		methods: {}
 	}
 </script>
 
 <style>
 
 </style>
-
